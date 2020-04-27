624c684edb77ee772370e6eceedb7d64d573c710
Tymothy Sellers <tsellers1990@gmail.com>
2020-04-27 10:11:40 -0600

    initial framework for the questions path

--- index.js
+++ index.js
@@ -47,9 +47,109 @@ const initilize = () => {
     //some code in here to initialize the good stuff
     //use inquirer then output the answer into some more stuff
     inquirer 
-        .prompt(q1Init)
+        .prompt(q1Init).then(function(answer){
+            switch (answer.baseInit){
+                case "Add":
+                    addFunction();
+                    break;
+                case "View":
+                    viewFunction();
+                    break;
+                case "Update":
+                    updateFunction();
+                    break;
+            }
+        })
 
-}//.then(function(answer))
+}
+
+const addFunction = () => {
+    inquirer 
+    .prompt(addQ).then(function(answer){
+        switch (answer.addInit){
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+        }
+    })
+}
+
+const viewFunction = () => {
+    inquirer 
+    .prompt(viewQ).then(function(answer){
+        switch (answer.viewInit){
+            case "View Department":
+                viewDepartment();
+                break;
+            case "View Role":
+                viewRole();
+                break;
+            case "View Employee":
+                viewEmployee();
+                break;
+        }
+    })  
+}
+
+const updateFunction = () => {
+    inquirer 
+    .prompt(updateQ).then(function(answer){
+        switch (answer.updateInit){
+            case "Update Department":
+                updateDepartment();
+                break;
+            case "Update Role":
+                updateRole();
+                break;
+            case "Update Employee":
+                updateEmployee();
+                break;
+        }
+    })
+}
+
+const addDepartment = () => {
+ 
+}
+
+const addRole = () => {
+    
+}
+
+const addEmployee = () => {
+    
+}
+
+const viewDepartment = () => {
+
+}
+
+const viewRole = () => {
+    
+}
+
+const viewEmployee = () => {
+    
+}
+
+const updateDepartment = () => {
+    
+}
+
+const  updateRole = () => {
+    
+}
+
+const updateEmployee = () => {
+    
+}
+
 
 
 //we'll fs need to have a shitload of functions here. this is a big project
